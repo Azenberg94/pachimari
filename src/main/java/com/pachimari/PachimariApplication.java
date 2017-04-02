6f107f510408130217cdbd147158fefb98f684c7
Azenberg94 <azedine.naidja@yahoo.fr>
2017-04-02 11:15:50 +0200

    Initial commit

---
+++ src/main/java/com/pachimari/PachimariApplication.java
@@ -0,0 +1,12 @@
+package com.pachimari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PachimariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PachimariApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pachimari/PachimariApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pachimari;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PachimariApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
