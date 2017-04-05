2a9f429d4005c2cb94eea4a0c044e0967b380c7b
Azenberg94 <azedine.naidja@yahoo.fr>
2017-04-05 19:15:36 +0200

    Codeship test n°2

--- src/main/java/com/pachimari/PachimariApplication.java
+++ src/main/java/com/pachimari/PachimariApplication.java
@@ -2,9 +2,19 @@ package com.pachimari;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class PachimariApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PachimariApplication.class, args);
