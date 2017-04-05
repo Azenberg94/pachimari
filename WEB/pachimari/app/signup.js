2d3c7a0480122bf185608605d4cd0763d4128c1f
goasguenl <loic.goasguen@mail-alpha.com>
2017-04-05 16:20:13 +0200

    le node c izi

--- WEB/pachimari/app/signup.js
+++ WEB/pachimari/app/signup.js
@@ -3,7 +3,11 @@ module.exports = function(app, models){
     var passwordHash = require('password-hash');
     var msgError="";
     var bcrypt = require('bcrypt-nodejs');
+	var rp = require('request-promise')
 	var request = require('request');
+	var requestoption1;
+	var requestoption2;
+	
 	var api = models.myApi; 
  
 	// =====================================
@@ -11,8 +15,7 @@ module.exports = function(app, models){
 	// =====================================
 	// show the signup form
 	app.get('/signup', function(req, res, next) {
-		var api = models.myApi; 
-		
+
 		// render the page and pass in any flash data if it exists
 		res.render('signup.ejs', {msgError:""});
 	});
@@ -22,25 +25,88 @@ module.exports = function(app, models){
 
         if (!req.body.username){
             msgError = "Veuillez saisir un login !"
+			res.render('signup.ejs', {msgError:msgError});
         }else if (!req.body.password){
             msgError = "Veuillez saisir un mot de passe !"
+			res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.passwordConfirm){
             msgError = "Veuillez retaper votre mot de passe"
+			res.render('signup.ejs', {msgError:msgError});
         }else if(req.body.password!=req.body.passwordConfirm){
             msgError = "Les mots de passe saisient ne sont pas identiques !"
+			res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.firstName){
             msgError = "Veuillez saisir votre nom ! "
+			res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.name){
              msgError = "Veuillez saisir votre prénom ! "
+			 res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.mail){
              msgError = "Veuillez saisir votre mail ! "  
+			 res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.adresse){
              msgError = "Veuillez saisir votre adresse ! "  
+			 res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.cp){
              msgError = "Veuillez saisir votre CP ! "  
+			 res.render('signup.ejs', {msgError:msgError});
         }else if(!req.body.ville){
              msgError = "Veuillez saisir votre ville ! "  
+			 res.render('signup.ejs', {msgError:msgError});
         }else{
+			
+			rp("http://"+api.host+"/user/login/"+req.body.username).then(function(body){
+				if(body){
+					res.render('signup.ejs', {msgError:"Cet utilisateur existe déjà !"});
+					next();
+				}
+			}).catch(function (err) {
+					console.log("Error rp1 : " + err)
+			}).then(function(){
+				rp({
+					url: "http://"+api.host+"/user/" ,
+					method: "POST",
+					headers:{ 
+						'Content-Type': 'application/json'
+					},
+					json:{ 
+					  "lastName": req.body.name,
+					  "name":req.body.firstName,
+					  "login": req.body.username,
+					  "email": req.body.mail,
+					  "adresse" : req.body.adresse,
+					  "cp" : req.body.cp,
+					  "ville" : req.body.ville,
+					  "type": "user"
+
+						}
+				}).then(function(body){
+					console.log("Body 1 : " + body)
+				}).catch(function (err) {
+					console.log("Error body1 : " + err)
+				});
+				
+				rp({
+					url: "http://"+api.host+"/auth/add/" ,
+					method: "POST",
+					headers:{ 
+						'Content-Type': 'application/json'
+					},
+					json:{ 
+					  "login": req.body.username,
+					  "pwd": req.body.password
+					}
+				}).then(function(body){
+					console.log("Body 2 : " + body)
+				}).catch(function (err) {
+					console.log("Error body2 : " + err)
+				});
+				
+			}).then(function(){
+				res.render('signup.ejs', {msgError:"Inscription validé !"});
+			});
+			
+			/*
 			request.get(
 				"http://"+api.host+"/user/login/"+req.body.username ,
 				function (error, response, body) {
@@ -50,7 +116,7 @@ module.exports = function(app, models){
 						request({
 							url: "http://"+api.host+"/user/" ,
 							method: "POST",
-						 headers:{ 
+							headers:{ 
 								'Content-Type': 'application/json'
 							},
 							json:{ 
@@ -94,12 +160,12 @@ module.exports = function(app, models){
 					}
 				}
 			);
-
+*/
         }
 
 
         
-        res.render('signup.ejs',{msgError: msgError});
+        
         
 
 	});
