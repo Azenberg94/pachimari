85fd399b03611818c7dce7a3cd55c3813b05e7dd
Azenberg94 <azedine.naidja@yahoo.fr>
2017-04-05 19:37:27 +0200

    Codeship test n°2 (develop branch)

--- src/main/java/com/pachimari/PachimariApplication.java
+++ src/main/java/com/pachimari/PachimariApplication.java
@@ -6,12 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class PachimariApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PachimariApplication.class, args);
