b5b6c5a357ee6af2320f21f05895044ec543be99
Pierre <oxeyo@hotmail.fr>
2017-04-05 22:10:05 +0100

    pull

--- src/test/java/com/pachimari/MongoConfigTest.java
+++ src/test/java/com/pachimari/MongoConfigTest.java
@@ -1,22 +1,15 @@
 package com.pachimari;
 
 
+import com.mongodb.Mongo;
 import cz.jirutka.spring.embedmongo.EmbeddedMongoBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import com.mongodb.Mongo;
-
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @Configuration
 public class MongoConfigTest extends  AbstractMongoConfiguration {
-    @Autowired
-    private Environment env;
 
     @Override
     public String getDatabaseName() {
