219b60a2c4eabff581aaa0424d698dad71362119
Azenberg94 <azedine.naidja@yahoo.fr>
2017-04-11 13:24:09 +0200

    SQL Model to MongoDB Model

--- src/main/java/com/pachimari/item/model/ItemEntity.java
+++ src/main/java/com/pachimari/item/model/ItemEntity.java
@@ -1,8 +1,7 @@
-package com.pachimari.user;
+package com.pachimari.item.model;
 
 import com.pachimari.order.model.OrderEntity;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,32 +9,21 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-
-/**
- * Created by Pierre on 11/03/2017.
- */
-
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "user")
-public class User {
-
-    @Id
+@Document(collection="item")
+public class ItemEntity {
+    @org.springframework.data.annotation.Id
     @GeneratedValue
-    private String id;
+    private int itemId;
 
-    private String name;
+    private String itemName;
 
-    private String email;
-
-    private String login;
-
-    private String type;
+    private Float itemPrice;
 
     @DBRef
-    private Set<OrderEntity> orders = new HashSet<>(0);
+    private Set<OrderEntity> orders = new HashSet<>();
 }
-
--- src/main/java/com/pachimari/order/model/OrderEntity.java
+++ src/main/java/com/pachimari/order/model/OrderEntity.java
@@ -1,36 +1,30 @@
 package com.pachimari.order.model;
 
-import com.pachimari.item.model.Item;
+import com.pachimari.item.model.ItemEntity;
 import com.pachimari.user.User;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@Entity
-@ToString(exclude = "user")
-@EqualsAndHashCode(exclude = {"user", "items"})
-@Table(name = "orders")
+@Document(collection = "order")
 public class OrderEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "order_id")
+    @GeneratedValue
     private long orderId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id", nullable = false)
+    @DBRef
     private User user;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "orders_items",
-            joinColumns = { @JoinColumn(name = "order_id", nullable = false, updatable = false)},
-            inverseJoinColumns = { @JoinColumn(name = "item_id", nullable = false, updatable = false)}
-    )
-    private Set<Item> items = new HashSet<>(0);
+    @DBRef
+    private Set<ItemEntity> items = new HashSet<>(0);
 }
