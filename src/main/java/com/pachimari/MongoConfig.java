c5759f215e52d1e46e6e425a9d309f14a084bdc0
Azenberg94 <azedine.naidja@yahoo.fr>
2017-04-26 16:20:22 +0200

    Functional version

--- src/main/java/com/pachimari/MongoConfig.java
+++ src/main/java/com/pachimari/MongoConfig.java
@@ -1,6 +1,5 @@
 package com.pachimari;
 
-import com.mongodb.MongoClientURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -13,13 +12,13 @@ public class MongoConfig extends  AbstractMongoConfiguration {
 
     @Override
     public String getDatabaseName() {
-        return "heroku_ncgf20l8";
+        return "pachimari";
     }
 
     @Override
     @Bean
     public Mongo mongo() throws Exception {
-        return new MongoClient(new MongoClientURI("mongodb://az:projetjee2017@ds117931.mlab.com:17931/heroku_ncgf20l8"));
+        return new MongoClient("127.0.0.1");
     }
 }
 
